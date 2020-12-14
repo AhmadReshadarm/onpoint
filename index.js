6583fc3a9ebbcda21ed4575ac0b3445c6e84c3b4
Rishad Mohammadi <armreshad@gmail.com>
2020-12-15 02:52:43 +0300

    onpoint project test mobile done

--- index.js
+++ index.js
@@ -61,10 +61,17 @@ slids.addEventListener("wheel", () => {
 slids.addEventListener("touchmove", () => {
   slider.value = slidsOffSet;
 });
-
+window.addEventListener("change", () => {
+  if (window.innerWidth < 1025) {
+    slider.max = 1500;
+    if (window.innerWidth < 450) {
+      slider.max = 600;
+    }
+  }
+});
 if (window.innerWidth < 1025) {
   slider.max = 1500;
   if (window.innerWidth < 450) {
-    slider.max = 500;
+    slider.max = 600;
   }
 }
